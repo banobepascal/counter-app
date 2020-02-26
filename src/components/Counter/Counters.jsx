eaf35be00979551999920cc35a49e40d78c541e3
banobepascal <banobepascal@gmail.com>
2020-02-26 17:18:59 +0300

    ch(refactoring counters): moved functions to App
    - changed from classes to functional components
    - used prop parentheses
    
    Finishes

--- src/components/Counter/Counters.jsx
+++ src/components/Counter/Counters.jsx
@@ -1,17 +1,23 @@
-import React, { Component } from "react";
+import React from "react";
 import Counter from "./Counter";
 
-class Counters extends Component {
-  render() {
-    return (
-      <div>
-        <Counter />
-        <Counter />
-        <Counter />
-        <Counter />
-      </div>
-    );
-  }
-}
+const Counters = props => {
+  return (
+    <div>
+      <button onClick={props.onReset} className="btn btn-primary btn-sm m-2">
+        Reset
+      </button>
+      {props.counters.map(counter => (
+        <Counter
+          key={counter.id}
+          onDelete={props.onDelete}
+          onIncrement={props.onIncrement}
+          onDecrement={props.onDecrement}
+          counter={counter}
+        />
+      ))}
+    </div>
+  );
+};
 
 export default Counters;

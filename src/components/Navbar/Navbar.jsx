5d692c204ff6e3e1c830331157ec541f74ed3ec1
banobepascal <banobepascal@gmail.com>
2020-02-26 16:31:55 +0300

    ch(navbar component): added navbar to App
    Finishes

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+
+const Navbar = props => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        Navbar{" "}
+        <span className="badge badge-pill badge-secondary">
+          {props.totalCounters}
+        </span>
+      </a>
+    </nav>
+  );
+};
+
+export default Navbar;

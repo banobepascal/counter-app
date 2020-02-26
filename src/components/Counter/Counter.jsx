ddde6db01731457874c0117dcd4ef4bd81e6567b
banobepascal <banobepascal@gmail.com>
2020-02-26 11:52:59 +0300

    ft(creating counter component)
    Finishes

---
+++ src/components/Counter/Counter.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Counter extends Component {
+
+  render() {
+
+    return (
+      <div>
+        <button className="btn btn-secondary btn-sm">increment</button>
+      </div>
+    );
+  }
+
+ 
+}
+
+export default Counter;

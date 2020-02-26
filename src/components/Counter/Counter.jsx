76406169985431dfa78e000add35c0074038e950
banobepascal <banobepascal@gmail.com>
2020-02-26 16:38:51 +0300

    ch(added new elements): modified counter.jsx
    Finishes

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -2,23 +2,18 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0,
-    tags: ["tag1", "tag2", "tag3"]
+    value: this.props.counter.value
   };
 
   formatCount = () => {
-    const { count } = this.state;
-    return count === 0 ? "zero" : count;
+    const { value } = this.props.counter;
+    return value === 0 ? "zero" : value;
   };
 
   getBadgeClasses = () => {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
-  };
-
-  incrementHandler = () => {
-    this.setState({ count: this.state.count + 1 });
   };
 
   render() {
@@ -26,10 +21,22 @@ class Counter extends Component {
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.incrementHandler}
-          className="btn btn-secondary btn-sm"
+          onClick={() => this.props.onIncrement(this.props.counter)}
+          className="btn btn-secondary btn-sm m-2"
         >
-          increment
+          +
+        </button>
+        <button
+          onClick={() => this.props.onDecrement(this.props.counter)}
+          className="btn btn-secondary btn-sm m-2"
+        >
+          -
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+          className="btn btn-danger btn-sm m2"
+        >
+          Delete
         </button>
       </div>
     );

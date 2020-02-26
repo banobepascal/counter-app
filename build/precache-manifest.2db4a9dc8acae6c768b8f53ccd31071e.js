b2b06a432ed7e5419e6ae4e933a73535fb0cc2e6
banobepascal <banobepascal@gmail.com>
2020-02-26 11:49:28 +0300

    initial commit

---
+++ build/precache-manifest.2db4a9dc8acae6c768b8f53ccd31071e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9ba47fa658cf82259e4080000399961",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc999a37436588ba08a8",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b49ede963a0f2ff5b4ef",
+    "url": "/static/js/2.62dce5ae.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.62dce5ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc999a37436588ba08a8",
+    "url": "/static/js/main.cbf62320.chunk.js"
+  },
+  {
+    "revision": "8963f78c0e1a10b5862f",
+    "url": "/static/js/runtime-main.8b07e775.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);

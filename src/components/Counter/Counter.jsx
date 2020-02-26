e9ae0fc59363a89745dd655023aeccc358e6bcee
banobepascal <banobepascal@gmail.com>
2020-02-26 12:00:16 +0300

    ft(rendering counter): dynamic counter class
    - added state to counter
    - added dynamic functions to render state
    - added bootstrap classes
    - made a code refactor
    
    Finishes

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,11 +1,26 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  state = {
+    count: 0
+  };
+
+  formatCount = () => {
+    const { count } = this.state;
+    return count === 0 ? "zero" : count;
+  };
+
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+}
 
   render() {
 
     return (
       <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">increment</button>
       </div>
     );

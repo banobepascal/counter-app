2adb7c9315057ea945cc5f243ba7e21c05d9f38b
banobepascal <banobepascal@gmail.com>
2020-02-26 13:16:19 +0300

    ft(increment handler): added function to increment count
    Finishes

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,8 +1,10 @@
 import React, { Component } from "react";
+import uuid from "uuid";
 
 class Counter extends Component {
   state = {
-    count: 0
+    count: 0,
+    tags: ["tag1", "tag2", "tag3"]
   };
 
   formatCount = () => {
@@ -10,23 +12,41 @@ class Counter extends Component {
     return count === 0 ? "zero" : count;
   };
 
-  getBadgeClasses() {
+  getBadgeClasses = () => {
     let classes = "badge m-2 badge-";
     classes += this.state.count === 0 ? "warning" : "primary";
     return classes;
-}
+  };
+
+  incrementHandler = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  renderTags = () => {
+    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+    return (
+      <ul>
+        {this.state.tags.map(tag => (
+          <li key={uuid.v4()}>{tag}</li>
+        ))}
+      </ul>
+    );
+  };
 
   render() {
-
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">increment</button>
+        <button
+          onClick={this.incrementHandler}
+          className="btn btn-secondary btn-sm"
+        >
+          increment
+        </button>
+        {this.renderTags()}
       </div>
     );
   }
-
- 
 }
 
 export default Counter;
